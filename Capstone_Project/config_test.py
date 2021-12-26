6926a9a01ebde24b0fc4ad96ecb4053fdae973e1
Apple <apple@Apples-MacBook-Pro.local>
2021-12-25 21:32:48 -0800

    Moved files to respective locations

--- Capstone_Project/config_test.py
+++
@@ -1,9 +0,0 @@
-import configparser
-import ast
-# Below code is working
-config = configparser.RawConfigParser()
-config.read('config.cfg')
-    
-details_dict = dict(config.items('geocoding_mapping'))
-x = ast.literal_eval(details_dict["column_list"])
-print(x)
